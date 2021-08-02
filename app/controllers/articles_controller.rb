4ccfd3205fbf31b9a25c5140b9fb887c76650d8d
cmanish049 <cmanish049@gmail.com>
2021-08-02 17:24:35 +0545

    changes after delete

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -4,4 +4,49 @@ class ArticlesController < ApplicationController
         #byebug
         @article = Article.find(params[:id])
     end
+
+    def index
+        @articles = Article.all
+    end
+
+    def new
+        @article = Article.new
+    end
+
+    def create
+        #byebug
+        @article = Article.new(params.require(:article).permit(:title, :description))
+        if @article.save
+            flash[:notice] = "Article was created successfully."
+            redirect_to @article
+        else
+            render 'new'
+        end
+    end
+
+    def edit
+        @article = Article.find(params[:id])
+    end
+
+    def update
+        @article = Article.find(params[:id])
+        if @article.update(params.require(:article).permit(:title, :description))
+            flash[:notice] = "Article was updated successfully."
+            redirect_to @article
+        else
+            render 'edit'
+        end
+    end
+
+    def destroy
+        @article = Article.find(params[:id])
+        if @article.destroy
+            flash[:notice] = "Article was deleted successfully."
+            redirect_to articles_path
+        else
+            flash[:notice] = "Unable to delete article."
+            redirect_to articles_path
+        end
+    end
+
 end
